7626edadb915160611ef7ac62145421de7965e3a
RITIK-02 <77235013+RITIK-02@users.noreply.github.com>
2021-05-03 01:57:57 +0530

    Update knn.py

--- knn.py
+++ knn.py
@@ -4,6 +4,8 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
 
 
 #In CSV file seniors=1, fourth=0
@@ -30,8 +32,9 @@ y_train = str_to_numConverter(train_data.iloc[:,2].values)
 x_test = test_data.iloc[:,[0,1]].values
 y_test = str_to_numConverter(test_data.iloc[:,2].values)
 
+k = 5
 #Creating model
-model = KNeighborsClassifier(n_neighbors = 5)
+model = KNeighborsClassifier(n_neighbors = k)
 model.fit(x_train,y_train)
 
 #Predicting values from that model
@@ -41,28 +44,24 @@ print("Accuracy :", round(accuracy_score(y_test, y_pred),4))
 print()
 cm = confusion_matrix(y_test,y_pred)
 print('Statistics:')
+print('Value of k used :',k)
 print('Total number of results predicted : ',len(y_pred))
-print('Number of results correctly matched : ',cm[0][0]+cm[1][1])
+print('Number of results correctly predicted : ',cm[0][0]+cm[1][1])
 print('Number of results incorrectly predicted : ',cm[0][1]+cm[1][0])
 
 
 
-'''
+
 #For plotting a graph
-import matplotlib.pyplot as plt
-from matplotlib.colors import ListedColormap
 x_set, y_set = x_test, y_test
-X1, X2 = np.meshgrid(np.arange(start = x_set[:, 0].min() - 1, stop = x_set[:, 0].max() + 1, step = 0.01),
-                     np.arange(start = x_set[:, 1].min() - 1, stop = x_set[:, 1].max() + 1, step = 0.01))
-plt.contourf(X1, X2, model.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+X1, X2 = np.meshgrid(np.arange(start = x_set[:, 0].min() - 1, stop = x_set[:, 0].max() + 1, step = 0.01),np.arange(start = x_set[:, 1].min() - 1, stop = x_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, model.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),alpha = 0.75, cmap = ListedColormap(('red', 'green')))
 plt.xlim(X1.min(), X1.max())
 plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
-    plt.scatter(x_set[y_set == j, 0], x_set[y_set == j, 1],
-                c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('KNN Classification')
+    plt.scatter(x_set[y_set == j, 0], x_set[y_set == j, 1],c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('KNN Classification (k='+str(k)+')')
 plt.xlabel('Shoe Size')
 plt.ylabel('Height')
 plt.legend()
-plt.show()'''
+plt.show()
